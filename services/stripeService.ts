63d1be95af67c2aee0936511a5503eab6d722d7c
shirzad <155540649+hamidshirzad@users.noreply.github.com>
2025-11-14 01:37:53 +0100

    Update stripeService.ts

--- services/stripeService.ts
+++ services/stripeService.ts
@@ -103,3 +103,12 @@ export const stripeService = {
     };
   }
 };
+<script async
+  src="https://js.stripe.com/v3/buy-button.js">
+</script>
+
+<stripe-buy-button
+  buy-button-id="buy_btn_1STB1EFFlyQJmhYsE1AjKBE4"
+  publishable-key="pk_live_51OfDuBFFlyQJmhYsiSrtcyTp1AIdjTSkBtToM3xaoa95YDnSE1LGmtYpd9IBLv0ESCKnVAuMhXscb2M3g9CGPs8J00Sfg64306"
+>
+</stripe-buy-button>
